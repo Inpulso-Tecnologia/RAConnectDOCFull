56d963af4fc5adff7e5e2ea0cf26128d69408ce8
Patrick Albino <patrick.lucca@inpulso.com.br>
2020-01-16 13:47:23 -0200

    Revisão do PDF

--- docs/conf.py
+++ docs/conf.py
@@ -145,6 +145,7 @@ latex_elements = {
     # The font size ('10pt', '11pt' or '12pt').
     #
     'pointsize': '30pt',
+    'fontsize': '24pt',
 
     # Additional stuff for the LaTeX preamble.
     #
